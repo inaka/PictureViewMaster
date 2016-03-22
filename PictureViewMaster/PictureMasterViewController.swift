e2fda2685d63db99d51064795267cf5488fbedb0
The Gera Of The People <Gerardo@Inakas-MacBook-Air.local>
2016-03-22 15:35:12 -0300

    Added code to reset the image to fit the screen. Added code to center the image. Added different sizes and ratios test images

--- PictureViewMaster/PictureMasterViewController.swift
+++ PictureViewMaster/PictureMasterViewController.swift
@@ -17,7 +17,29 @@ class PictureMasterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.addGestureRecognizers()
-        self.imageViewFrame = self.imageView.frame
+    }
+    
+    private func originalImageViewFitFrameForImage(image: UIImage) -> CGRect {
+        let screenSize = UIScreen.mainScreen().bounds
+        var frame = CGRect()
+        
+        let heightRatio = image.size.height / image.size.width
+        let widthRatio = image.size.width / image.size.height
+        let screenRatio = screenSize.width / screenSize.height
+        
+        if image.size.height > image.size.width && widthRatio < screenRatio {
+            
+            frame.size.height = screenSize.height
+            frame.size.width = frame.height * widthRatio
+        }else {
+            frame.size.width = screenSize.width
+            frame.size.height = frame.width * heightRatio
+        }
+        
+        frame.origin.x = (screenSize.size.width / 2) - (frame.size.width / 2)
+        frame.origin.y = (screenSize.size.height / 2) - (frame.size.height / 2)
+        
+        return frame
     }
     
     private func addGestureRecognizers() {
@@ -33,7 +55,7 @@ class PictureMasterViewController: UIViewController {
         let rotateRecognizer = UIRotationGestureRecognizer(target: self, action: Selector("rotateGesture:"))
         self.imageView.addGestureRecognizer(rotateRecognizer)
         
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("tapGesture:"))
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: Selector("tapBackgroundGesture:"))
         self.backgroundView.addGestureRecognizer(tapRecognizer)
     }
     
@@ -45,6 +67,11 @@ class PictureMasterViewController: UIViewController {
     }
     
     func dragGesture(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .Ended {
+            self.centerImage(self.imageView)
+            return
+        }
+        
         guard let view = gesture.view else { return }
     
         let translation = gesture.translationInView(self.view)
@@ -53,18 +80,33 @@ class PictureMasterViewController: UIViewController {
         gesture.setTranslation(CGPointZero, inView: self.view)
     }
     
-    func rotateGesture (gesture: UIRotationGestureRecognizer) {
+    func rotateGesture(gesture: UIRotationGestureRecognizer) {
         guard let view = gesture.view else { return }
         
         view.transform = CGAffineTransformRotate(view.transform, gesture.rotation)
         gesture.rotation = 0
     }
     
-    func tapGesture (gesture: UITapGestureRecognizer){
+    func tapBackgroundGesture(gesture: UITapGestureRecognizer) {
+        self.resetImageFrameAndRotation(self.imageView)
+    }
+    
+    private func centerImage(imageView: UIImageView) {
         UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: {
-            let rotation = (atan2(self.imageView.transform.b, self.imageView.transform.a))
-            self.imageView.transform = CGAffineTransformRotate(self.imageView.transform, -rotation)
-            self.imageView.frame = self.imageViewFrame
+            let screenSize = UIScreen.mainScreen().bounds
+            let centerX = (screenSize.size.width / 2) - (imageView.frame.width / 2)
+            let centerY = (screenSize.size.height / 2) - (imageView.frame.height / 2)
+            
+            let frame = CGRect(x: centerX, y: centerY, width: imageView.frame.width, height: imageView.frame.height)
+            imageView.frame = frame
+            }, completion:nil)
+    }
+    
+    private func resetImageFrameAndRotation(imageView: UIImageView) {
+        UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: {
+            let rotation = (atan2(imageView.transform.b, imageView.transform.a))
+            imageView.transform = CGAffineTransformRotate(imageView.transform, -rotation)
+            imageView.frame = self.originalImageViewFitFrameForImage(imageView.image!)
             }, completion:nil)
     }
 }
